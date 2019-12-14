670ce35ff5488c35a9ffea6cb6d32b4036a00b8f
peterckim <peterckim96@gmail.com>
2019-12-14 18:49:34 -0500

    solved house-robber

--- src/Bloomberg.java
+++ src/Bloomberg.java
@@ -417,4 +417,15 @@ public class Bloomberg {
             }
         }
     }
+
+    public static int rob(int[] nums) {
+        return rob(nums, nums.length - 1);
+    }
+
+    private static int rob(int[] nums, int i) {
+        if (i < 0) {
+            return 0;
+        }
+        return Math.max(rob(nums, i - 2) + nums[i], rob(nums, i - 1));
+    }
 }
