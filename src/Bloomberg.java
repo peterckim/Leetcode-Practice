42a8c6aa2820fbdb363628f494fb13c30b1b61a5
peterckim <peterckim96@gmail.com>
2019-12-14 15:41:14 -0500

    solved maxAreaOfIsland

--- src/Bloomberg.java
+++ src/Bloomberg.java
@@ -361,4 +361,30 @@ public class Bloomberg {
 
         return finalString.reverse().toString();
     }
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int max_area = 0;
+        /*
+            Step 1: Find all the lands
+            Step 2: Check starting with the biggest whether it is an island
+         */
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    max_area = Math.max(max_area, areaOfIsland(grid, i, j));
+                }
+            }
+        }
+        return max_area;
+    }
+
+    public int areaOfIsland(int[][] grid, int i, int j) {
+        if (i >= 0 && i < grid.length && j >= 0 && j < grid[i].length && grid[i][j] == 1) {
+            grid[i][j] = 0;
+            return 1 + areaOfIsland(grid, i + 1, j) + areaOfIsland(grid, i - 1, j) + areaOfIsland(grid, i, j + 1) + areaOfIsland(grid, i, j - 1);
+        }
+
+        return 0;
+    }
 }
