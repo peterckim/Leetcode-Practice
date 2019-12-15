a081c44e2b7300932e054ca40af8dd8cb654eeb5
peterckim <peterckim96@gmail.com>
2019-12-15 12:27:46 -0500

    solved getIntersectionNode

--- src/Bloomberg.java
+++ src/Bloomberg.java
@@ -483,4 +483,43 @@ public class Bloomberg {
 
         return count;
     }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        /*
+            Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+            Output: Reference of the node with value = 8
+            Input Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
+                From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,0,1,8,4,5].
+                There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+         */
+
+        /* Brute Force */
+        /* Loop through each and save each node in a Set */
+
+        Set<ListNode> listNodeLookupTable = new HashSet();
+
+
+        while (headA != null) {
+            listNodeLookupTable.add(headA);
+            if (headA.next == null) {
+                break;
+            } else {
+                headA = headA.next;
+            }
+        }
+
+        while (headB != null) {
+            if (listNodeLookupTable.contains(headB)) {
+                return headB;
+            } else {
+                if (headB.next == null) {
+                    break;
+                } else {
+                    headB = headB.next;
+                }
+            }
+        }
+
+        return null;
+    }
 }
