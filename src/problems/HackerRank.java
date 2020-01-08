a4313e2c2a7cc8eeffe32f558619e539a5debc57
peterckim <peterckim96@gmail.com>
2020-01-07 20:43:47 -0500

    adding comments

--- src/problems/HackerRank.java
+++ src/problems/HackerRank.java
@@ -1,11 +1,19 @@
 package problems;
 
+import java.math.BigInteger;
 import java.util.*;
 
 public class HackerRank {
-    public static void main(String[] args) {
-    }
+    public static void main(String[] args) {}
 
+    /**
+     * @problem     Climbing the Leaderboard
+     * @link        https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem?h_r=internal-search
+     * @difficulty  Medium
+     * @param       scores -> [100, 90, 90, 80, 75, 60]
+     * @param       alice -> [50, 65, 77, 90, 102]
+     * @return      res -> 6
+     */
     public static int[] climbingLeaderboard(int[] scores, int[] alice) {
         int[] res = new int[alice.length];  /* The result will be the same length as alice Array, so instantiate it */
         int index = 0;  /* Using an index to not loop through the entire scores array every time. Instead loop from where we left off. */
@@ -36,121 +44,26 @@ public class HackerRank {
         return res;
     }
 
+    /**
+     * @problem     Extra Long Factorials
+     * @link        https://www.hackerrank.com/challenges/extra-long-factorials/problem?h_r=internal-search
+     * @difficulty  Medium
+     * @param       n -> 25
+     * @return      res -> 15511210043330985984000000
+     */
     public static void extraLongFactorials(int n) {
-        long dp[] = new long[n];
-        dp[0] = 1;
+        /* instantiate dynamic programming memo array */
+        BigInteger dp[] = new BigInteger[n];
 
+        /* first value will be 1 indicating 1! */
+        dp[0] = BigInteger.ONE;
+
+        /* build up dp array */
         for (int i = 1; i < n; i++) {
-            dp[i] = dp[i - 1] * (i + 1);
+            dp[i] = dp[i - 1].multiply(BigInteger.valueOf(i + 1));
         }
 
         System.out.println(dp[n - 1]);
     }
-
-    public static int nonDivisibleSubset(int k, List<Integer> s) {
-
-        return nonDivisibleSubset(k, s, new ArrayList<Integer>());
-    }
-
-    static int nonDivisibleSubset(int k, List<Integer> s, List<Integer> res) {
-        System.out.println("s: " + s);
-        System.out.println("res: " + res);
-
-        if (s.isEmpty()) {
-            System.out.println("empty");
-            /* check if res is max */
-            int sum = 0;
-            for (int i = 0; i < res.size(); i++) {
-                sum += res.get(i);
-            }
-
-            if (sum % k != 0) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
-
-
-        res.add(s.get(0));
-
-
-        return Math.max(1 + nonDivisibleSubset(k, s.subList(1, s.size()), res), nonDivisibleSubset(k, s.subList(1, s.size()), res.subList(0, res.size() - 1)));
-    }
-
-//    static int queensAttack(int n, int k, int r_q, int c_q, int[][] obstacles) {
-//        int[] closest_obstacles = new int[8];
-//        int row = r_q;
-//        int column = c_q;
-//
-//        for (int i = 0; i < obstacles.length; i++) {
-//            for (int j = 0; j < obstacles[i].length; j++) {
-//
-//            }
-//        }
-//
-//
-//    }
-
-    static void checkMagazine(String[] magazine, String[] note) {
-        Map<String, Integer> wordBank = new HashMap();
-        String flag = "Yes";
-
-        for (String s: magazine) {
-            wordBank.put(s, wordBank.getOrDefault(s, 0) + 1);
-        }
-
-        for (String s: note) {
-            if (!wordBank.containsKey(s)) {
-                flag = "No";
-                break;
-            }
-            if (wordBank.get(s) >= 1) {
-                wordBank.put(s, wordBank.get(s) - 1);
-            } else {
-                flag = "No";
-                break;
-            }
-        }
-
-
-
-        System.out.println(flag);
-    }
-
-    public static int sherlockAndAnagrams(String s) {
-        /*
-            "ifailuhqq"
-            "ifa, fai, i, i, q, q"
-         */
-
-        char[] sToArray = s.toCharArray();
-        Set<String> wordBank = new HashSet();
-        int count = 0;
-
-        for (int i = 0; i < sToArray.length; i++) {
-            if (i == sToArray.length - 1) {
-                if (sToArray[i] == sToArray[i - 1]) {
-                    count++;
-                }
-            }
-            for (int j = i + 1; j < sToArray.length; j++) {
-                StringBuilder sbInner = new StringBuilder();
-                char[] subArray = Arrays.copyOfRange(sToArray, i, j);
-                Arrays.sort(subArray);
-                for (char c: subArray) {
-                    sbInner.append(c);
-                }
-                if (wordBank.contains(sbInner.toString())) {
-                    System.out.println("second if: " + sbInner.toString());
-                    count++;
-                }
-                wordBank.add(sbInner.toString());
-            }
-        }
-
-        return count;
-    }
-
 }
 
