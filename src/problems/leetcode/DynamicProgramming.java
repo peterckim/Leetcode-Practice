1e251c247a79ce4dcb75dbcdefb27a554f8feebd
peterckim <peterckim96@gmail.com>
2020-01-08 15:41:12 -0500

    adding comments

--- src/problems/leetcode/DynamicProgramming.java
+++ src/problems/leetcode/DynamicProgramming.java
@@ -20,9 +20,15 @@ public class DynamicProgramming {
      * @topdown
      */
     public static int climbStairs(int n) {
+        /*
+            Base Cases
+                if n reaches negative, it is not a valid path
+                if n reaches 0, it is a valid path
+        */
         if (n < 0) return 0;
         if (n == 0) return 1;
 
+        /* Recursive Case */
         return (climbStairs(n - 2) + climbStairs(n - 1));
     }
 
@@ -36,19 +42,53 @@ public class DynamicProgramming {
      * @bottomup
      */
     public static int climbStairsBottomUp(int n) {
+        /*
+            Base Cases
+                if n reaches negative, it is not a valid path
+                if n reaches 0, it is a valid path
+        */
         if (n < 0) return 0;
         if (n == 0 || n == 1) return 1;
 
-
+        /* Initialize dp memoization array */
         int[] dp = new int[n + 1];
-
         dp[0] = 1;
         dp[1] = 1;
 
+        /* Bottom Up DP */
         for (int i = 2; i <= n; i++) {
             dp[i] = dp[i - 2] + dp[i - 1];
         }
 
         return dp[n];
+    }
+
+    /**
+     * @problem     322. Coin Change
+     * @link        https://leetcode.com/problems/coin-change/
+     * @tags        [Dynamic Programming]
+     * @difficulty  Medium
+     * @param       coins -> []
+     * @param       amount ->
+     * @return      res -> 3
+     * @topdown
+     */
+    public static int coinChange(int[] coins, int amount) {
+        /* Math.min(coinChange(1), coinChange(2), coinChange(3)); */
+        return 0;
+    }
+
+    /**
+     * @problem     322. Coin Change
+     * @link        https://leetcode.com/problems/coin-change/
+     * @tags        [Dynamic Programming]
+     * @difficulty  Medium
+     * @param       coins -> []
+     * @param       amount ->
+     * @return      res -> 3
+     * @bottomup
+     */
+    public static int coinChangeBottomUp(int[] coins, int amount) {
+        return 0;
     }
 }
