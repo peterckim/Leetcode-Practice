dd61f27a821ec1dcc2f1511a1903a0286bdd80b8
peterckim <peterckim96@gmail.com>
2020-01-07 20:08:05 -0500

    added linear search

--- src/search/LinearSearch.java
+++ src/search/LinearSearch.java
@@ -1,4 +1,26 @@
 package search;
 
+/**
+ * In computer science, a linear search or sequential search is a method for finding an element within a list.
+ *  It sequentially checks each element of the list until a match is found or the whole list has been searched.
+ *
+ * Time Complexities:
+ *      worst-case -> O(N)
+ *      best-case -> O(1)
+ *      average-case -> O(N)
+ *
+ * Space Complexities:
+ *      worst-case -> O(1)
+ *
+ * @see <a href="https://en.wikipedia.org/wiki/Linear_search">Linear Search (Wikipedia)</a>
+ */
+
 public class LinearSearch {
+    public static final int find(int value, int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value) return i;
+        }
+
+        return -1;
+    }
 }
